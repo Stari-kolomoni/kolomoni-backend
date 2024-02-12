94b9dcc64f639c95c2fff93ec47882ebcda6ad99
Simon Goričar <simon.peter.goricar@gmail.com>
2024-02-12 19:58:29 +0100

    feat: remove now-unused `user_permission` table in favor of `user_role` (work in progress)

--- kolomoni_migrations/src/m20230624_170512_initialize_permission_related_tables.rs
+++ kolomoni_migrations/src/m20230624_170512_initialize_permission_related_tables.rs
@@ -26,23 +26,6 @@ const PERMISSION_IDX_ON_NAME_INDEX_NAME: &str = "index__permission__on__name";
 const PERMISSION_UNIQUE_ON_NAME_CONSTRAINT_NAME: &str = "unique__permission__name";
 
 
-#[derive(DeriveIden)]
-pub enum UserPermission {
-    #[sea_orm(iden = "user_permission")]
-    Table,
-
-    #[sea_orm(iden = "user_id")]
-    UserId,
-
-    #[sea_orm(iden = "permission_id")]
-    PermissionId,
-}
-
-const USER_PERMISSION_PK_CONSTRAINT_NAME: &str = "pk__user_permission";
-const USER_PERMISSION_FK_USER_ID: &str = "fk__user_permission__user_id";
-const USER_PERMISSION_FK_PERMISSION_ID: &str = "fk__user_permission__permission_id";
-
-
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -109,57 +92,10 @@ impl MigrationTrait for Migration {
                     .unique()
                     .to_owned(),
             )
-            .await?;
-
-
-        manager
-            .create_table(
-                Table::create()
-                    .table(UserPermission::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new_with_type(UserPermission::UserId, ColumnType::Integer)
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new_with_type(UserPermission::PermissionId, ColumnType::Integer)
-                            .not_null(),
-                    )
-                    .primary_key(
-                        Index::create()
-                            .name(USER_PERMISSION_PK_CONSTRAINT_NAME)
-                            .col(UserPermission::UserId)
-                            .col(UserPermission::PermissionId),
-                    )
-                    .foreign_key(
-                        ForeignKey::create()
-                            .name(USER_PERMISSION_FK_USER_ID)
-                            .from(UserPermission::Table, UserPermission::UserId)
-                            .to(User::Table, User::Id)
-                            .on_delete(ForeignKeyAction::Cascade)
-                            .on_update(ForeignKeyAction::Cascade),
-                    )
-                    .foreign_key(
-                        ForeignKey::create()
-                            .name(USER_PERMISSION_FK_PERMISSION_ID)
-                            .from(
-                                UserPermission::Table,
-                                UserPermission::PermissionId,
-                            )
-                            .to(Permission::Table, Permission::Id)
-                            .on_delete(ForeignKeyAction::Cascade)
-                            .on_update(ForeignKeyAction::Cascade),
-                    )
-                    .to_owned(),
-            )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .drop_table(Table::drop().table(UserPermission::Table).to_owned())
-            .await?;
-
         manager
             .drop_table(Table::drop().table(Permission::Table).to_owned())
             .await
