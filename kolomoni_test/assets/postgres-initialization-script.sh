37665463f12a4f9d8f09e01bdc69ed0340eef3c2
Simon Goričar <simon.peter.goricar@gmail.com>
2024-02-17 22:16:23 +0100

    fix: fix PostgreSQL permission issue in end-to-end tests

--- kolomoni_test/assets/postgres-initialization-script.sh
+++ kolomoni_test/assets/postgres-initialization-script.sh
@@ -3,5 +3,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE ROLE kolomoni WITH PASSWORD 'kolomoni' LOGIN;
-	GRANT ALL PRIVILEGES ON DATABASE kolomoni TO kolomoni;
+	GRANT ALL ON DATABASE kolomoni TO kolomoni;
+	ALTER DATABASE kolomoni OWNER TO kolomoni;
 EOSQL
