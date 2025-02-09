e282015dd438f179d59e65cb97a1e42ced54dd0f
Simon Goričar <simon.peter.goricar@gmail.com>
2025-02-09 17:04:22 +0100

    fix: fix unstable --report-time and --ensure-time testing flags, fix import warning in kolomoni_core

--- kolomoni_core/src/api_models/testing.rs
+++ kolomoni_core/src/api_models/testing.rs
@@ -1,16 +1,16 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use uuid::Uuid;
 
 
 #[derive(Deserialize, PartialEq, Eq, Debug)]
-#[cfg_attr(feature = "serde_impls_for_client_on_models", derive(Serialize))]
+#[cfg_attr(feature = "serde_impls_for_client_on_models", derive(serde::Serialize))]
 pub struct GiveAdministratorRoleRequest {
     pub user_id: Uuid,
 }
 
 
 #[derive(Deserialize, PartialEq, Eq, Debug)]
-#[cfg_attr(feature = "serde_impls_for_client_on_models", derive(Serialize))]
+#[cfg_attr(feature = "serde_impls_for_client_on_models", derive(serde::Serialize))]
 pub struct ResetUserRolesRequest {
     pub user_id: Uuid,
 }
