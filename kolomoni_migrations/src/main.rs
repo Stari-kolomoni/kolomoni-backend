81b7041c58a073a9ddf2bf6ee681fdb5692935ca
Simon Goričar <simon.peter.goricar@gmail.com>
2025-02-09 16:41:05 +0100

    fix: fix forgotten todo!() macro panic in kolomoni_migrate CLI

--- kolomoni_migrations/src/main.rs
+++ kolomoni_migrations/src/main.rs
@@ -22,21 +22,19 @@ fn load_dotenv_files_if_exist() -> Result<()> {
     let dotenv_result = dotenvy::dotenv();
 
     if let Err(dotenv_err) = dotenv_result {
-        match dotenv_err {
+        return match dotenv_err {
             dotenvy::Error::Io(error) => match error.kind() {
-                ErrorKind::NotFound => todo!(),
-                _ => {
-                    return Err(error)
-                        .into_diagnostic()
-                        .wrap_err("failed to load any dotenv file")
-                }
+                ErrorKind::NotFound => Ok(()),
+                _ => Err(error)
+                    .into_diagnostic()
+                    .wrap_err("failed to load any dotenv file"),
             },
             error => {
                 return Err(error)
                     .into_diagnostic()
                     .wrap_err("failed to load any dotenv file")
             }
-        }
+        };
     }
 
     Ok(())
@@ -58,14 +56,13 @@ pub fn main() -> Result<()> {
 }
 
 
-
-
-// TODO Required CLI commands:
+// TODO CLI commands:
 // - [DONE, needs a style pass] initialize: creates the migration directory if needed
 // - [DONE, needs a style pass] generate: generates a new empty migration (runs initialize automatically if needed)
-// - [PENDING, medium priority] fresh: drops all tables from the database and reapplies all migrations
-// - [PENDING, low priority] refresh: rolls back all migrations, then reapplies all of them
-// - [PENDING, low priority] reset: rolls back all migrations
-// - [PENDING, high priority] status: displays the status of all migrations, both applied or not
+// - [TODO, medium priority] fresh: drops all tables from the database and reapplies all migrations
+//                           (this might be too complicated because we have privileged migrations)
+// - [TODO, low priority] refresh: rolls back all migrations, then reapplies all of them
+// - [TODO, low priority] reset: rolls back all migrations
+// - [DONE, needs a style pass] status: displays the status of all migrations, both applied or not
 // - [DONE, needs a style pass] up: applies all pending migrations (or up to a specific version)
 // - [DONE, needs a style pass] down: rolls back to a specific database version (migration version)
